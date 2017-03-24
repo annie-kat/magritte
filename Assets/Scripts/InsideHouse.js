8e8981712fa1980164889155110da19b5ff1899c
annie-kat <annekalford@gmail.com>
2017-03-23 20:59:45 -0400

    Created basic sandbox. Fixed skybox. Starting work on door prefab.
    This is a test.

---
+++ Assets/Scripts/InsideHouse.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+public var as1 : AudioSource;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (other : Collider){
+	as1.volume = 0.5;
+}
+
+//Deactivate the Main function when player is go away from door
+function OnTriggerExit (other : Collider){
+	as1.volume = 0.9;
+}
